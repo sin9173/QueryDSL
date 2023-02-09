db44bd4d2b0965a17e13bece404ea0e947aa68ae
sin9173 <sid9173@gmail.com>
2023-02-09 22:01:05 +0900

    Setting, Model

---
+++ src/test/java/study/querydsl/QueryDSLBasicTest.java
@@ -0,0 +1,19 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+public class QueryDSLBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    
+
+}
